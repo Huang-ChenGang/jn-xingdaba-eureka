b745b3c80d04cc61b2b4965d5c817b5f8eca4c58
Huang ChenGang <hcggang@163.com>
2020-12-20 14:51:11 +0800

    eureka start success

---
+++ src/main/java/com/jn/xingdaba/eureka/JnXingdabaEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.jn.xingdaba.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class JnXingdabaEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnXingdabaEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jn/xingdaba/eureka/JnXingdabaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jn.xingdaba.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnXingdabaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
